51afb51260637dd1a6e1d0fcdde08ac653f36f59
jkciesluk <323892@uwr.edu.pl>
2021-01-24 22:30:21 +0100

    Its working!

--- head.h
+++ head.h
@@ -4,7 +4,8 @@
 #define RES 9
 
 void UpdateEnemies(char board[RES][RES]);
-
+extern char P[155][155];
+extern char t[9][9];
 extern char sym[8];
 WINDOW *create_window(int height, int width, int posy, int posx, bool border);
 void print_room(WINDOW *win, char room[9][9]);
